fdea570c690e920aa3e74fcb7cd39b6981cd7423
Dane Bista <danebista>
2022-01-13 15:27:58 +0545

    Longest Valid Paranthesis

---
+++ 32. LongestValidParanthesis.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack();
+        stack.push(-1);
+        int max = 0;
+        for(int i=0; i< s.length(); i++){
+            if (s.charAt(i)=='(') stack.push(i);
+            else {
+                stack.pop();
+                if (stack.isEmpty()){
+                    stack.push(i);
+                }
+                else{
+                 int len = i - stack.peek();
+                 max = Math.max(len, max);
+                }
+            }
+        }
+         return max;
+    }
+   
+}
+  
