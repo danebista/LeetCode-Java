db64535a466b163a2c12eadd07a4a688a0d8a6ac
Dane Bista <danebista>
2022-01-22 21:16:25 +0545

    98

---
+++ 91-100/98. Valid BST.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> stack = new Stack();
+        double left_val = -Double.MAX_VALUE;
+        while (!stack.isEmpty() || root !=null){
+            while (root !=null){
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.val <= left_val) return false;
+            left_val = root.val;
+            root = root.right;
+            
+        }
+        return true;
+    }
+}
