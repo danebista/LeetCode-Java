c8b96a53749bf2c6ea1f824fbfee035fb76f0ed5
Dane Bista <danebista>
2022-02-07 21:39:05 +0545

    Palindrome partitioning

--- 131-140/131. Palindrome Partitioning I.java
+++ 131-140/131. Palindrome Partitioning I.java
@@ -2,34 +2,34 @@ class Solution {
     boolean[][] isPalin;
     
     public List<List<String>> partition(String s) {
-        isPalin = new boolean [s.length()][s.length()];
         List<List<String>> result = new ArrayList();
-        helper(result, new ArrayList(), 0, s);
+        helper(result, new ArrayList(), s, 0);
         return result;
     }
     
-    public void helper(List<List<String>> result, List<String> current, int i, String s){
-        if (i==s.length()){
+    public void helper(List<List<String>> result, List<String> current, 
+                      String s, int i){
+        if (i == s.length()){
             result.add(new ArrayList(current));
             return;
         }
         
-        for (int j=i; j<s.length(); j++){
-            if (isPalindrome(s,i,j)){
-                current.add(s.substring(i, j+1));
-                helper(result, current, j+1, s);
+        for (int j=i; j<s.length();j++){
+            if(isPalindrome(s,i,j)){
+                current.add(s.substring(i,j+1));
+                helper(result, current, s, j+1);
                 current.remove(current.size()-1);
             }
         }
-        
     }
     
     public boolean isPalindrome(String s, int i, int j){
-        if (isPalin[i][j]) return true;
         while(i<=j){
-            if (s.charAt(i++)!=s.charAt(j--)) return false;
+           if (s.charAt(i++) != s.charAt(j--)) return false;  
         }
         
-        return isPalin[i][j];
+        return true;
     }
+    
+     
 }
