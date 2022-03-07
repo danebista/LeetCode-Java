de43a2f2017e97c4a5c2cdd15700c58feb2dd3cf
Dane Bista <danebista>
2022-03-07 13:49:13 +0545

    Paint House II

--- 261-270/262. Trips and Users.java
+++ 261-270/262. Trips and Users.java
@@ -4,5 +4,4 @@ Round(Sum(If(status!='completed',1,0))/Count(request_at),2)  AS 'Cancellation Ra
 where request_at between '2013-10-01' and '2013-10-03'
 and client_id not in(Select users_id from Users where banned='Yes')
 and driver_id not in(select users_id from Users where banned='Yes')
-  group by request_at
-  
+group by request_at
